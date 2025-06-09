0b68df2a42285ed626617685cf318d90d531335b
Andrik <andrik.gtz.rguez@gmail.com>
2025-06-08 20:01:21 -0600

    ordinario-ftw

---
+++ html00/dist/xyzM.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y: "+o),"fin y"}function r(o){return n("x"),console.log("x: "+o),"fin x"}function e(o){return"fin x"}function l(o){return console.log("z: "+o),"fin z"}document.getElementById("app");console.log(r("abc"));console.log(e("def"));console.log(n("ghi"));console.log(l("jkl"));})();
+//# sourceMappingURL=xyzM.js.map
